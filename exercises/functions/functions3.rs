e74ba7bfcf652f7327681158776582a19699e8cd
Ye <fengyun.ye@kpmg.com>
2022-09-02 15:05:33 +0800

    init

---
+++ exercises/functions/functions3.rs
@@ -0,0 +1,13 @@
+// functions3.rs
+// Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
+
+
+fn main() {
+    call_me();
+}
+
+fn call_me() {
+    for i in 0..3 {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
