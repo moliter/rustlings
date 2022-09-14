a3fa05383316c3018691b3c13b79e7f0fdbed6ee
moliter <molitermoliter@outlook.com>
2022-09-14 16:44:22 +0800

    thread

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -29,18 +29,20 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc1 = qc.clone();
     let qc2 = qc.clone();
 
+    let tx1 = tx.clone();
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
+    let tx2 = tx.clone();
     thread::spawn(move || {
         for val in &qc2.second_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx2.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
